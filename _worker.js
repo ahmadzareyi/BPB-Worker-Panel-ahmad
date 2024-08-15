349d6ecf84ea63cae78fa7c866d09588cefce60a
NotRwinShadow <137303887+Kwinshadow@users.noreply.github.com>
2024-08-15 20:30:31 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9,14a6b396-c2c3-43b6-8b4b-fa67e2c1b001';
 let vipUserID = '14a6b396-c2c3-43b6-8b4b-fa67e2c1b001';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
